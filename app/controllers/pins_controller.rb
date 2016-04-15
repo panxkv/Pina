a8a973c4f3c2211f3aa4988abf2bf7dd16df4aab
panxkv <kamil.kiedrzyn@gmail.com>
2016-04-15 23:50:56 +0200

    Add meth and views  to create Pin.

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,23 @@
+class PinsController < ApplicationController
+    def index
+    end
+
+    def new
+      @pin = Pin.new
+    end
+
+    def create
+      @pin = Pin.new(pin_params)
+
+      if @pin.save
+        redirect_to @pin, notice: "Pin created"
+      else
+        render 'new'
+    end
+
+    private
+
+    def pin_params
+      params.require(:pin).permit(:title, :description)
+    end
+end
