db683807d84baec3675862d4bac60ab75e10b446
panxkv <kamil.kiedrzyn@gmail.com>
2016-04-18 22:04:37 +0200

    Make Pin CRUDE!

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,11 @@
 class PinsController < ApplicationController
+    before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
     def index
+      @pins = Pin.all.order("created_at DESC")
+    end
+
+    def show
     end
 
     def new
@@ -13,6 +19,23 @@ class PinsController < ApplicationController
         redirect_to @pin, notice: "Pin created"
       else
         render 'new'
+      end
+    end
+
+    def edit
+    end
+
+    def update
+    	if @pin.update(pin_params)
+    		redirect_to @pin, notice: "Pin was successfully updated"
+    	else
+    			render 'edit'
+    	end
+    end
+
+    def destroy
+      @pin.destroy
+      redirect_to root_path
     end
 
     private
@@ -20,5 +43,8 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:title, :description)
     end
-end
+
+    def find_pin
+      @pin = Pin.find(params[:id])
+    end
 end
